f78713461e1863a00a432cd42b06f1033394b473
MelPeslier <mel.peslier@gmail.com>
2023-08-13 12:37:40 -0300

    server side addons + global.gd as autoload + ClientWolf as autoload

---
+++ autoloads/global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var YOUR_SILENTWOLF_API_KEY :String = "dnJ0rZvJDI3Vfz6wrJ2Dx9diJiAKZ0wy7HJeGwLO"
+var YOUR_SILENTWOLF_GAME_ID :String = "FloristKing"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SilentWolf.configure({
+		"api_key": YOUR_SILENTWOLF_API_KEY,
+		"game_id": YOUR_SILENTWOLF_GAME_ID,
+		"log_level": 1
+	})
+
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/MainPage.tscn"
+	})
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
