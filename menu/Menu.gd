2265502b7eda3aab6e6c4d88191853576d87561a
GarettSkott <wilton.hisbury@gmail.com>
2023-08-15 16:17:19 +0200

    Menu 1.0

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -2,15 +2,33 @@ extends CanvasLayer
 
 const startScene = preload("res://shop/night_shop.tscn")
 # Change the scene with the day
+const menuMusic = preload("res://audio/music/MenuTheme.ogg")
+
+func _ready():
+	Music.stream = menuMusic
+	Music.play()
 
 func _on_play_pressed():
 	get_tree().change_scene_to_packed(startScene)
 
 
 func _on_leaderboards_pressed():
+	$Credits/CreditsPanel.visible = false
 	# Mel will deal with this
+	# How about the list appears in the 
+	#  same place the credits appear?
 	pass
+
+func _on_credits_pressed():
+	$Credits/CreditsPanel.visible = !$Credits/CreditsPanel.visible
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
+
+
+func _on_mouse_entered():
+	$Audio.play()
