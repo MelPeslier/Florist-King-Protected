f6e4ee2d0420c8fd2ec769d2a7d2ce73c45b51b3
GarettSkott <wilton.hisbury@gmail.com>
2023-08-15 10:14:49 +0200

    a

--- player/Player.gd
+++ player/Player.gd
@@ -7,17 +7,6 @@ func _ready():
 	position = currentRoom.position
 	currentRoom.enter()
 
-func _process(_delta):
-	# Movement
-	if Input.is_action_just_pressed("ui_left"):
-		currentRoom.move(0, self)
-	elif Input.is_action_just_pressed("ui_right"):
-		currentRoom.move(1, self)
-	elif Input.is_action_just_pressed("ui_up"):
-		currentRoom.move(2, self)
-	elif Input.is_action_just_pressed("ui_down"):
-		currentRoom.move(3, self)
-
 
 func _on_move_cooldown_timeout():
 	canMove = true
